54a16ad1f6da4f308daac7d124aed3c362e93f49
MOJTABAFA <ms47188@uga.edu>
2015-11-29 02:46:51 +0330

    Syntax debugging applied

--- dictLearningSpark.py
+++ dictLearningSpark.py
@@ -4,11 +4,9 @@ import numpy as np
 from io import StringIO   # StringIO behaves like a file object
 import math
 import random
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from numpy import linalg as sla
-
-
-# RAND_MAX = 2147483647
+RAND_MAX = 2147483647
 
 def op_selectTopR( vct_input, idxs_n, R):
 	temp = np.argpartition(-vct_input, R)
@@ -26,9 +24,8 @@ def op_getResidual( S, u, v, I, idxs_n, R):
 	for i in range (I):
 		for idx_r in range(R):
 			j = idxs_n[idx_r]
-			S[[i], [j]] = S[[i], [j]] - u[i]*v[j]
-	return(S)		
-	
+			S[[i],[j]] = S[[i],[j]] - u[i]*v[j]
+	return (S)
 def main():
 #parser = argparse.ArgumentParser(description='close bug')
 	parser = argparse.ArgumentParser(description = 'PySpark Dictionary Learning',
@@ -45,7 +42,7 @@ def main():
         help = "Length of the samples.")
 	parser.add_argument("-P", "--pnumber", type = int, required = True,
         help = "Number of the samples.")
-	parser.add_argument("-n", "--pnonzero", type = int, required = True,
+	parser.add_argument("-n", "--pnonzero", type = float, required = True,
         help = "Percentage of Non-zero elements.")
 	parser.add_argument("-m", "--mDicatom", type = int, required = True,
         help = "Number of the dictionary atoms.")
@@ -54,50 +51,47 @@ def main():
 
 	args = vars(parser.parse_args())
 
-# Intializing the variables ;
 	P = int(args['pnumber'])
 	T = int(args['length'])
 	M = int(args['mDicatom'])
 	PCT = float(args['pnonzero'])
 	R = float(PCT * P)
 	epsilon = float(args['epsilon'])
-	file_summary = args['summary']
-    file_s = args['input']
-    file_D = args['dictionary']
-    file_Z = args['output']
-
+	file_s = str(args['input'])
+	file_D = str(args['dictionary'])
+	file_summary = str(args['summary'])
+	file_Z = str(args['output'])
 # setting the max number of iterations
 	max_iteration = P*10
 # Opening the file in Write mode & converting the TXT file to a matrix
 	print("The Input file is loading...")
     #S = np.genfromtxt(file_s,delimiter='    ') 
-    S = np.loadtxt(file_s)
+	S = np.loadtxt(file_s)
 # Normalizing the Data 
 	S = S - S.mean(axis=0)
-#above line is instead of "stat_normalize2zeroMeanMTX( S, T, P )"
+#stat_normalize2zeroMeanMTX( S, T, P )
 	S = S / sla.norm(S)
-#above line is instead of "stat_normalize2l2NormMTX( S, T, P )"
+#stat_normalize2l2NormMTX( S, T, P )
 	print('Training .... \n')
-# Initializing 4 vectors with zero    
-    u_old = np.zeros((1,T), dtype=np.float)
-    u_new = np.zeros((1,T), dtype=np.float)
-    v = np.zeros((1,P), dtype=np.float)
-    idxs_n = np.zeros((1,R), dtype=np.float)
-    print ('Initalization is complete!')
-    epsilon = epsilon * epsilon
-# main loop
+# Initializing 4 vectors with zero
+	u_old = np.zeros((1,T), dtype = np.float)
+	u_new = np.zeros((1,T), dtype = np.float)
+	v = np.zeros((1,P), dtype=np.float)
+	idxs_n = np.zeros((1,R), dtype=np.float)
+	print('Initalization is complete!')
+	epsilon = epsilon * epsilon
+
 	for m in range(M):
 		it=0
 		u_old = np.random.random(T)
-		u_old = u_old - u_old.mean(axis=0)
+		#stat_randVCT( u_old, T )
 		#above instruction is instead of "stat_normalize2zeroMeanVCT( u_old, T )"
-		u_old = u_old / sla.norm(u_old)
+		u_old = u_old - u_old.mean(axis=0)
 		#above instruction is instead of "stat_normalize2l2NormVCT( u_old, T )"
-
+		u_old = u_old / sla.norm(u_old)
 		print('Analyzing component ',(m+1),'...')
 		
 		while True :
-		
 			# this instruction is equal with : op_VCTbyMTX( S, u_old, v, T, P );
 			v = np.dot(S,u_old)
 			idxs_n= op_selectTopR(v,idxs_n,R)
