bcc445b2e5190fa7f9f1006b22336d1363e1d501
Shannon Quinn <magsol@gmail.com>
2015-12-19 19:40:32 -0500

    Updated some of the argument options to clarify their purposes.

--- R1DL_Spark.py
+++ R1DL_Spark.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 
 from pyspark import SparkContext, SparkConf
 
@@ -6,21 +7,28 @@ if __name__ == "__main__":
     # Set up the arguments here.
     parser = argparse.ArgumentParser(description = 'PySpark Dictionary Learning',
         add_help = 'How to use', prog = 'python pyspark-example.py <args>')
+
+    # Inputs.
     parser.add_argument("-i", "--input", required = True,
-        help="Input File name.(file_s)")
-    parser.add_argument("-d", "--dictionary", required = True,
-        help="Dictionary File name.(file_D)")
-    parser.add_argument("-o", "--output", required = True,
-        help="Output File name.(file_Z)")
+        help = "Input file containing the matrix S.")
     parser.add_argument("-n", "--pnonzero", type = float, required = True,
-        help="Percentage of Non-zero elements.")
+        help = "Percentage of Non-zero elements.")
     parser.add_argument("-m", "--mDicatom", type = int, required = True,
-        help="Number of the dictionary atoms.")
+        help = "Number of the dictionary atoms.")
     parser.add_argument("-e", "--epsilon", type = float, required = True,
-        help="The value of epsilon.")
+        help = "The value of epsilon.")
+
+    # Outputs.
+    parser.add_argument("-d", "--dictionary", required = True,
+        help = "Output path to dictionary file.(file_D)")
+    parser.add_argument("-o", "--output", required = True,
+        help = "Output path to Z matrix.(file_Z)")
 
     args = vars(parser.parse_args())
 
     # Initialize the SparkContext. This is where you can create RDDs,
     # the Spark abstraction for distributed data sets.
     sc = SparkContext(conf = SparkConf())
+
+    # Read the raw data off HDFS.
+    raw_data = sc.textFile(args['input'])
