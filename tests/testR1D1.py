f20ea07220bf93dbd835e8db7d8066a0c36424b7
Shannon Quinn <magsol@gmail.com>
2016-01-19 17:14:06 -0500

    Added initial unit testing (which fails) using the testSets 1 and 2.

--- tests/testR1D1.py
+++ tests/testR1D1.py
@@ -1,9 +1,31 @@
+import numpy as np
+import os
 import unittest
+
+from R1DL import r1dl
 
 class TestR1D1(unittest.TestCase):
 
-    def test_selectTopR(self):
+    def test_dataSet1(self):
+        self._runTest("../testSet1")
+
+    def test_dataSet2(self):
+        self._runTest("../testSet2")
+
+    def test_largeData(self):
         pass
+
+    def _runTest(self, path, R = 0.2, M = 5, epsilon = 0.01, sigfigs = 6):
+        S = np.loadtxt(os.path.join(path, "S.txt"))
+        Ztrue = np.loadtxt(os.path.join(path, "z_groundtruth.txt"))
+        Dtrue = np.loadtxt(os.path.join(path, "D_groundtruth.txt"))
+
+        # Run the test.
+        D, Z = r1dl(S, R, M, epsilon)
+
+        # Compare the results.
+        np.testing.assert_array_almost_equal(Z, Ztrue, decimal = sigfigs)
+        np.testing.assert_array_almost_equal(D, Dtrue, decimal = sigfigs)
 
 if __name__ == "__main__":
     unittest.main()
